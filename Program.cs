cda9ec051cc6d4efdda54b298e078de6da8b150a
manju priya <106304226+iammanjupriya@users.noreply.github.com>
2023-07-22 14:19:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    class Program
+    {
+        public int area(int r,float a)
+        {
+            float circle = r * r * a;
+            return (Convert.ToInt32 (circle));
+        }
+        public int area(int l , int w )
+        {
+            int rect = l * w ;
+            return rect;
+
+
+
+
+        }
+    }
+    class program1
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int s = p.area(4,3.14f);
+            Console.WriteLine("the area of circle is : " + s);
+            int s1 = p.area(10 , 20);
+            Console.WriteLine("the area of rectangle is  : " + s1);
+
+
+
+
+        }
+    }
+}
+
+
